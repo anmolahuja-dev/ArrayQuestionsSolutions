717c0eecd1b69d6f59ddd6a1a0062152a75461c9
anmolahuja-dev <anmolahuja359@gmail.com>
2021-06-13 01:57:34 +0530

    Common element in three sorted arrays

--- ProbSolving.cpp
+++ ProbSolving.cpp
@@ -9,35 +9,43 @@ using namespace std;
 #include"Merge.h"
 #include"MergeInterval.h"
 #include"nextPermutation.h"
+#include"getPairsCount.h"
 
-int getPairsCount(int arr[], int n, int k) {
-    
-    unordered_map<int, int>map;
-    for (int i = 0; i < n; i++) {
-        map[arr[i]] ++;
+vector <int> commonElements(int A[], int B[], int C[], int n1, int n2, int n3)
+{
+    vector<int>ans;
+    unordered_map<int, int>m;
+
+    for (int i = 0; i < n1; i++) {
+        m[A[i]]++;
+    }
+    for (int i = 0; i < n2; i++) {
+        m[B[i]]++;
+    }
+    for (int i = 0; i < n3; i++) {
+        m[C[i]]++;
     }
 
-    int twice_count=0;
-    
-    for (int i = 0; i < n; i++) {
-        twice_count += map[k - arr[i]];
-
-        // if (arr[i], arr[i]) pair satisfies the condition,
-        // then we need to ensure that the count is
-        // decreased by one such that the (arr[i], arr[i])
-        // pair is not considered
-        if (k - arr[i] == arr[i])
-            twice_count--;
+    auto it = m.begin();
+    while (it != m.end()) {
+        if (it->second>1) {
+            ans.push_back(it->first);
+        }
+        it++;
     }
 
-    // return the half of twice_count
-    return twice_count / 2;
-
+    return ans;
 }
 
 int main()
 {
-    int arr[] = { 1,1 };
-    int count=getPairsCount(arr, 2, 2);
-    cout << count;
+    int n1 = 3; int A[] = { 1, 2,3 };
+    int n2 = 3; int B[] = { 1, 2,3 };
+    int n3 = 3; int C[] = { 1, 2,3 };
+
+    vector<int>ans = commonElements(A, B, C, n1, n2, n3);
+
+    for (auto it : ans) {
+        cout << it;
+    }
 }
--- getPairsCount.h
+++ getPairsCount.h
@@ -1,1 +1,25 @@
 #pragma once
+int getPairsCount(int arr[], int n, int k) {
+
+    unordered_map<int, int>map;
+    for (int i = 0; i < n; i++) {
+        map[arr[i]] ++;
+    }
+
+    int twice_count = 0;
+
+    for (int i = 0; i < n; i++) {
+        twice_count += map[k - arr[i]];
+
+        // if (arr[i], arr[i]) pair satisfies the condition,
+        // then we need to ensure that the count is
+        // decreased by one such that the (arr[i], arr[i])
+        // pair is not considered
+        if (k - arr[i] == arr[i])
+            twice_count--;
+    }
+
+    // return the half of twice_count
+    return twice_count / 2;
+
+}
